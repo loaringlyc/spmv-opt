f9aba841db296d300719bf1dd8809af424e69903
loaringlyc <3043119496@qq.com>
2026-02-14 19:43:05 -0500

    add command for performance evaluation

--- run.sh
+++ run.sh
@@ -10,3 +10,8 @@ module load cuda
 
 nvcc spmv.cu -o spmv -lcusparse
 ./spmv -f shyy41
+
+nsys profile --stats=true ./spmv -f shyy41
+# ncu --target-processes=all --set full --launch-count 1 ./spmv -f shyy41
+ncu --set summary --launch-count 1 ./spmv -f shyy41
+ncu --metrics gpu__compute_memory_access_throughput.avg.pct_of_peak_sustained_elapsed,lts__t_sectors_srcunit_tex_queries_hit_rate.avg.pct,sm__warp_issue_stalled_long_scoreboard_per_warp_active.pct --launch-count 1 ./spmv -f shyy41
